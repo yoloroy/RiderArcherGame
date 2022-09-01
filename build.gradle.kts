d79325f7a98427017672f39ef6d25575b2eb8bcd
yoloroy <slart2108@gmail.com>
2022-09-01 09:38:32 +0300

    :rocket: add properties for executables

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,15 @@ plugins {
 
 korge {
 	id = "com.yoloroy.rider_archer_game"
+    versionCode = 1
     version = "0.1"
     exeBaseName = "rag"
     name = "RiderArcherGame"
+    title = name
     description = "Game about RiderArcher"
+    authorName = "yojick"
+    authorHref = "https://yojick.itch.io"
+    copyright = copyright.replace("unknown", "$authorName https://github.com/yoloroy/RiderArcherGame/blob/master/LICENSE")
 
 	targetJvm()
 	targetJs()
