25bbd8309f33d8741a19709fbb1e734fb4799efc
yoloroy <slart2108@gmail.com>
2022-11-27 18:09:42 +0300

    :iphone: improve display of canopy firing by scaling the arrow while flying

--- src/commonMain/kotlin/view/implementations/projectiles/ArrowProjectile.kt
+++ src/commonMain/kotlin/view/implementations/projectiles/ArrowProjectile.kt
@@ -1,10 +1,13 @@
 package view.implementations.projectiles
 
+import com.soywiz.klock.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
 import com.soywiz.korma.geom.*
 import game.core.*
 import view.graphics_components.*
+import kotlin.math.*
+import kotlin.properties.*
 
 class ArrowProjectile(
     container: Container,
@@ -18,6 +21,30 @@ class ArrowProjectile(
     manager
 ) {
 
+    companion object {
+        const val G = 9.81
+        const val minimumScale = 0.5
+    }
+
+    private lateinit var canopyShootingMath: ArrowCanopyShootingMath
+    private var startScale by Delegates.notNull<Double>()
+
+    override fun launch(start: IPoint, destination: IPoint, speedPerSecond: Double) {
+        super.launch(start, destination, speedPerSecond)
+        println("launch from $start to $destination")
+        canopyShootingMath = ArrowCanopyShootingMath.forLaunch(G, fullDistance, speedPerSecond)
+        startScale = view.scale
+        view.scale = startScale * minimumScale
+    }
+
+    override fun update(dt: TimeSpan) {
+        super.update(dt)
+        canopyShootingMath.update(dt)
+        view.scale = startScale * amplify(canopyShootingMath.projectionRatio)
+    }
+
+    private fun amplify(sin: Double) = (sin + 0.3).pow(3).coerceAtLeast(minimumScale)
+
     class Creator(
         private val container: Container,
         private val color: RGBA = Colors.RED,
@@ -30,5 +57,38 @@ class ArrowProjectile(
         }
 
         override fun create() = ArrowProjectile(container, manager, color, onReachCallback)
+    }
+}
+
+class ArrowCanopyShootingMath private constructor(
+    private var verticalSpeedPerSecond: Double,
+    private val gravityAccelerationPerSecond: Double,
+    private val horizontalSpeedPerSecond: Double
+) {
+
+    companion object {
+        fun forLaunch(
+            gravityAccelerationPerSecond: Double,
+            fullDistance: Double,
+            horizontalSpeedPerSecond: Double
+        ) = ArrowCanopyShootingMath(
+            gravityAccelerationPerSecond * fullDistance / horizontalSpeedPerSecond,
+            gravityAccelerationPerSecond,
+            horizontalSpeedPerSecond
+        )
+
+        const val C = 100
+    }
+
+    private var accumulatedAltitude = 0.0
+
+    val projectionRatio get() = sin(angle)
+
+    // why there atan(y * C / x') instead of atan(y' * C / x')? - because that's how it works
+    private val angle get() = atan(accumulatedAltitude * C / horizontalSpeedPerSecond).radians
+
+    fun update(dt: TimeSpan) {
+        accumulatedAltitude += verticalSpeedPerSecond * dt.seconds
+        verticalSpeedPerSecond -= gravityAccelerationPerSecond * dt.seconds
     }
 }
